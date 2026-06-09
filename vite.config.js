6b85a0ae2860cc71b494c7477d5201dc9b464718
Sebastian Will <sebastian.h.will@gmail.com>
2026-06-09 09:56:03 +0200

    chore: add vitest + vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/duplicate-finder/',
-  build: {
-    target: 'es2022',
-  },
-  worker: {
-    format: 'es',
+  build: { target: 'es2022' },
+  worker: { format: 'es' },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, // use existing public/manifest.json
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
+  test: {
+    environment: 'node',
   },
 })
